410e681a66840f71f206b9c3077ba22516317475
R4H4T69 <166297407+R4H4T69@users.noreply.github.com>
2024-04-17 23:02:30 +0600

    Update mystarts.py

--- ethon/mystarts.py
+++ ethon/mystarts.py
@@ -8,15 +8,15 @@ async def start_srb(event, st):
                       buttons=[
                               [Button.inline("SET THUMB.", data="set"),
                                Button.inline("REM THUMB.", data="rem")],
-                              [Button.url("DEV", url="t.me/MaheshChauhan")]])
+                              [Button.url("DEV", url="t.me/r4h4t_69")]])
                               
     
 async def vc_menu(event):
-    await event.edit("**VIDEO CONVERTOR v1.4**", 
+    await event.edit("**VIDEO CONVERTOR v1.0**", 
                     buttons=[
-                        [Button.inline("info.", data="info"),
+                        [Button.inline("INFO", data="info"),
                          Button.inline("SOURCE", data="source")],
-                        [Button.inline("NOTICE.", data="notice"),
-                         Button.inline("Main.", data="help")],
-                        [Button.url("DEVELOPER", url="t.me/MaheshChauhan")]])
+                        [Button.inline("NOTICE", data="notice"),
+                         Button.inline("MAIN", data="help")],
+                        [Button.url("DEVELOPER", url="t.me/r4h4t_69")]])
     
